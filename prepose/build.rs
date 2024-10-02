beb2169d8e75f1b9fcc80cf98165c13cc182837a
Mirco De Marchi <filippo.ziche@gmail.com>
2024-10-02 18:15:57 +0200

    feat: make build incremental and add run script
    Rust started to compile every library all the times, so I made the build incremental. I do not know why this has become a problem only now.
    There is also a problem with the jetson-inference and jetson-utils libraries, without LD_LIBRARY_PATH the engine does not work.
    For this reason i made a run.sh script to remember what to do

--- prepose/build.rs
+++ prepose/build.rs
@@ -4,6 +4,17 @@ fn main() {
 
     let out_dir = env::var("OUT_DIR").unwrap();
 
+    // Rebuild library if cpp code has changed
+    println!("cargo::rerun-if-changed=cpp/library.cpp");
+
+    // Link jetson-inference and jetson-utils
+    println!("cargo:rustc-link-search=native=/home/nvidia/Repositories/prepare-core/prepose/libs");
+    println!("cargo:rustc-link-lib=dylib=jetson-inference");
+    println!("cargo:rustc-link-lib=dylib=jetson-utils");
+
+    // Link c++ stdlib
+    println!("cargo:rustc-link-lib=dylib=stdc++");
+
     // Compile libpose
     cc::Build::new()
         .cpp(true)
@@ -16,12 +27,4 @@ fn main() {
     // Link libpose
     println!("cargo:rustc-link-search=native={}", out_dir);
     println!("cargo:rustc-link-lib=static=pose");
-
-    // Link jetson-inference and jetson-utils
-    println!("cargo:rustc-link-search=native=/home/nvidia/PREPARE/prepose/libs");
-    println!("cargo:rustc-link-lib=dylib=jetson-inference");
-    println!("cargo:rustc-link-lib=dylib=jetson-utils");
-
-    // Link c++ stdlib
-    println!("cargo:rustc-link-lib=dylib=stdc++");
 }
