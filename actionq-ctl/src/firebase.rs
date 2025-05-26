3c3525d8243e7443297a5659b2d26b7b0f0d33f5
Filippo Ziche <filippo.ziche@univr.it>
2025-05-26 11:11:26 +0200

    feat: add aqctl to main codebase

---
+++ actionq-ctl/src/firebase.rs
@@ -0,0 +1,169 @@
+use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
+use firestore::{
+    paths, FirestoreDb, FirestoreListenerTarget, FirestoreTempFilesListenStateStorage,
+};
+
+use actionq_motion::ParameterDescriptor;
+
+// Run an async block in place
+#[macro_export]
+macro_rules! sync_async {
+    ($rt:expr, $f:expr) => {
+        $rt.block_on(async { $f })
+    };
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Document {
+    request: Command,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct ExerciseReps {
+    exercise_id: String,
+    num_repetitions: u32,
+    /// Optional runtime parameters
+    parameters: Option<HashMap<String, f32>>
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(tag = "type")]
+enum Command {
+    SessionStart {
+        exercises: Vec<ExerciseReps>,
+        save: bool,
+    },
+    SetPlayState {
+        running: bool,
+    },
+    SessionEnd,
+}
+
+// Control the session of a patient
+pub struct SessionCtrl {
+    patient_id: String,
+    db: FirestoreDb,
+}
+
+impl SessionCtrl {
+    /// Create a session controller for a specific patient
+    pub async fn new(patient_id: &str, database_id: &str) -> Self {
+        Self {
+            patient_id: patient_id.into(),
+            db: FirestoreDb::new(database_id)
+                .await
+                .expect("unable to connect to firestore database"),
+        }
+    }
+
+    /// Modify the command document
+    async fn update_command(&self, request: Command) {
+        let _: Document = self
+            .db
+            .fluent()
+            .update()
+            .fields(paths!(Document::request))
+            .in_col("jetson")
+            .document_id(&self.patient_id)
+            .object(&Document { request })
+            .execute()
+            .await
+            .unwrap();
+    }
+
+    /// Stop an exercise
+    pub async fn set_play_state(&self, running: bool) {
+        self.update_command(Command::SetPlayState { running }).await;
+    }
+
+    // Run single exercise
+    pub async fn run_exercise(&self, exercise_id: String, num_repetitions: u32, parameters: Option<HashMap<String, f32>>) {
+        self.update_command(Command::SessionStart { 
+            save: false, 
+            exercises: vec![
+                ExerciseReps {
+                    exercise_id, num_repetitions, parameters
+                }
+            ]
+        }).await;
+    }
+
+    // Stop a session
+    pub async fn session_stop(&self) {
+        self.update_command(Command::SessionEnd).await;
+    }
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct ExerciseParameter {
+    name: String,
+    description: String,
+    default: f32
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Exercise {
+    name: String,
+    description: String,
+    parameters: Vec<ParameterDescriptor>,
+    gif: String,
+    fsm: String,
+}
+
+// Interact with the database
+pub struct DatabaseCtrl {
+    db: FirestoreDb,
+}
+
+impl DatabaseCtrl {
+    /// Create a database controller
+    pub async fn new(database_id: &str) -> Self {
+        Self {
+            db: FirestoreDb::new(database_id)
+                .await
+                .expect("unable to connect to firestore database"),
+        }
+    }
+
+    /// Insert a new exercise
+    pub async fn add_exercise_definition(
+        &self,
+        name: &str,
+        description: &str,
+        fsm: &str,
+        gif_uri: &str,
+        parameters: Vec<ParameterDescriptor>
+    ) {
+        let obj = Exercise {
+            name: name.into(),
+            description: description.into(),
+            fsm: fsm.into(),
+            gif: gif_uri.into(),
+            parameters
+        };
+
+        // Remove exercise if already present
+        let _ = self
+            .db
+            .fluent()
+            .delete()
+            .from("exercises")
+            .document_id(name)
+            .execute()
+            .await;
+
+        // Insert new
+        let _: Exercise = self
+            .db
+            .fluent()
+            .insert()
+            .into("exercises")
+            .document_id(name)
+            .object(&obj)
+            .execute()
+            .await
+            .expect("unable to insert new exercise definition");
+    }
+}
+
---
+++ actionq-ctl/src/main.rs
@@ -0,0 +1,140 @@
+use std::error::Error;
+use crate::{firebase::{DatabaseCtrl, SessionCtrl}};
+use clap::{Parser, Subcommand};
+
+use actionq_motion::LuaExercise;
+
+mod firebase;
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    /// Google project id
+    #[arg(long, default_value = "uvc-unisco")]
+    project: String,
+    /// In what scope are we working on
+    #[command(subcommand)]
+    context: ArgsContext,
+}
+
+#[derive(Subcommand)]
+enum ArgsContext {
+    /// Run singe exercise
+    Exercise {
+        /// Id of the patient inside the database
+        patient: String,
+        /// Id of the exercise inside the database
+        exercise: String,
+        /// Number of repetitions
+        repetitions: u32,
+        /// Runtime parameters
+        #[arg(short = 'p', value_parser = parse_key_val::<String, f32>)]
+        parameters: Vec<(String, f32)>
+    },
+
+    Reset {
+        /// Id of the patient of the session to reset
+        patient: String,
+    },
+
+    /// Insert data into the firebase database
+    Database {
+        /// What operation to do
+        #[command(subcommand)]
+        command: CmdsDatabase,
+    },
+}
+
+#[derive(Subcommand)]
+enum CmdsDatabase {
+    InsertExercise {
+        /// Name of the exercise
+        name: String,
+        /// Description of the exercise
+        description: String,
+        /// Path to the fsm json file
+        fsm: String,
+        /// URI to the gif of the exercise.
+        /// Example: gs://uvc-unisco.appspot.com/gif.webp
+        gif: String,
+    },
+}
+
+/// Parse a single key-value pair
+fn parse_key_val<T, U>(s: &str) -> Result<(T, U), Box<dyn Error + Send + Sync + 'static>>
+where
+    T: std::str::FromStr,
+    T::Err: Error + Send + Sync + 'static,
+    U: std::str::FromStr,
+    U::Err: Error + Send + Sync + 'static,
+{
+    let pos = s
+        .find('=')
+        .ok_or_else(|| format!("invalid KEY=value: no `=` found in `{s}`"))?;
+    Ok((s[..pos].parse()?, s[pos + 1..].parse()?))
+}
+
+fn main() {
+
+    // Create single thread runtime
+    let rt = tokio::runtime::Builder::new_current_thread()
+        .enable_time()
+        .enable_io()
+        .build()
+        .expect("unable to create single-thread tokio runtime");
+
+    let args = Cli::parse();
+
+    match args.context {
+        ArgsContext::Exercise {
+            patient,
+            exercise,
+            repetitions,
+            parameters,
+        } => {
+            let ctrl =
+            sync_async!(rt, SessionCtrl::new(&patient, &args.project).await);
+
+            let parameters = if parameters.len() != 0 {
+                Some(parameters.into_iter().collect())
+            }
+            else {
+                None
+            };
+
+            dbg!(&parameters);
+            sync_async!(rt, ctrl.run_exercise(
+                exercise, repetitions, parameters).await);
+        }
+        ArgsContext::Reset { patient } => {
+            let ctrl =
+                sync_async!(rt, SessionCtrl::new(&patient, &args.project).await);
+            sync_async!(rt, ctrl.session_stop().await);
+        }
+        ArgsContext::Database { command } => {
+            let ctrl = sync_async!(rt, DatabaseCtrl::new(&args.project).await);
+            match command {
+                // Load fsm and gif into exercise definition
+                CmdsDatabase::InsertExercise {
+                    name,
+                    description,
+                    fsm,
+                    gif,
+                } => {
+                    let fsm_data =
+                        std::fs::read_to_string(&fsm).expect("unable to read fsm file");
+
+                    // Create LuaExercise to extract metadata
+                    let exercise = LuaExercise::from_string(fsm_data.clone(), "NONE".to_string(), "NONE".to_string(), 0, &[])
+                        .expect("Unable to load FSM script");
+
+                    sync_async!(
+                        rt,
+                        ctrl.add_exercise_definition(&name, &description, &fsm_data, &gif, Vec::from_iter(exercise.default_parameters))
+                            .await
+                    );
+                }
+            }
+        }
+    }
+}
